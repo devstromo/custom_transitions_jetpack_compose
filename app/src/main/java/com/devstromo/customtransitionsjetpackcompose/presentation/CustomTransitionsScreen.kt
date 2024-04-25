d26f2628f9cb6fc2daa1ed07b5d6de6291ef810c
alemoles <alemolerio@gmail.com>
2024-04-24 23:03:21 -0300

    Custom Transitions Jetpack Compose | update general style

--- app/src/main/java/com/devstromo/customtransitionsjetpackcompose/presentation/CustomTransitionsScreen.kt
+++ app/src/main/java/com/devstromo/customtransitionsjetpackcompose/presentation/CustomTransitionsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -46,6 +47,7 @@ fun CustomTransitionsScreen(
 
     BoxWithConstraints(
         modifier = modifier
+            .padding(20.dp)
     ) {
 
         Text(
